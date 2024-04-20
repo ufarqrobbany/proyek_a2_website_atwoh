56078b0a1d2179b37ae362be06283a109ec68d9e
ufarqrobbany <faruqrobbany@gmail.com>
2024-04-21 04:00:29 +0700

    update

--- scrape_manga_myanimelist/spiders/MangaScraper.py
+++ scrape_manga_myanimelist/spiders/MangaScraper.py
@@ -22,13 +22,19 @@ class MangaScraper(scrapy.Spider):
         rank = response.css('span.numbers.ranked > strong::text').get()
         popularity = response.css('span.numbers.popularity > strong::text').get()
         rating = response.css('div.score-label::text').get()
+        type = response.xpath('//div[contains(span[@class="dark_text"], "Type:")]/a/text()').get()
+        volume = response.xpath('//div[contains(span[@class="dark_text"], "Volumes:")]/text()').get()
+        chapter = response.xpath('//div[contains(span[@class="dark_text"], "Chapters:")]/text()').get()
+        status = response.xpath('//div[contains(span[@class="dark_text"], "Status:")]/text()').get()
         genres = f(response, "Genres:", "Genre:")
         themes = f(response, "Themes:", "Theme:")
-        demographic = f(response, "Demographic:", "Demographics:")
+        demographic = f(response, "Demographics:", "Demographic:")
         publish = response.xpath('//div[contains(span[@class="dark_text"], "Published:")]/text()').get()
         authors_text = response.xpath('//div[@class="spaceit_pad"]/span[@class="dark_text"][contains(text(), "Authors")]/following-sibling::a/text()').getall()
         authors = [i.replace(", ", " ") for i in authors_text]
-        synopsis = response.css('span[itemprop="description"]::text').get()
+        decription_text = response.css('span[itemprop="description"]').get()
+        synopsis = scrapy.Selector(text=decription_text).xpath('//text()').getall()
+        synopsis = ' '.join(synopsis).replace('\r\n', '').replace('\n', '').replace('\r', '').replace('\t', '').strip()
 
         item = {
             'rank': rank[1:],
@@ -37,10 +43,14 @@ class MangaScraper(scrapy.Spider):
             'image_url': image_url,
             'rating': rating,
             'popularity': popularity.replace("#", ""),
+            'type': type.strip(),
+            'volumes': volume.strip(),
+            'chapters': chapter.strip(),
+            'status': status.strip(),
             'genres': genres,
             'themes': themes,
             'demographic': demographic,
-            'publish': publish[:14].strip().replace("  ", " "),
+            'publish': publish.strip().replace("  ", " "),
             'authors': authors,
             'synopsis': synopsis
         }
