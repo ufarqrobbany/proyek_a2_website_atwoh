068c1a8fa35c4a9326f1dcc2a847e016334e871a
ufarqrobbany <faruqrobbany@gmail.com>
2024-05-21 11:30:35 +0700

    update

--- scrape_skincare_tokopedia/main.py
+++ scrape_skincare_tokopedia/main.py
@@ -1,127 +1,124 @@
-# import tkinter as tk
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
-# import time
-# import datetime
-# import sklearn
-# import flask
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import google.generativeai
-# import cv2 as cv
+import time
+import os
+import json
 
-
-import pandas as pd
-
+# Initialize Chrome options
 chrome_options = Options()
-# chrome_options.add_argument("--headless") # Run Chrome in headless mode
-# chrome_options.add_argument("--start-minimized")  # Minimize the window
+# Uncomment the following lines to run Chrome in headless mode and minimized
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--start-minimized")
 
+# Start the Chrome driver
 driver = webdriver.Chrome(options=chrome_options)
 
 # Set implicit wait to 10 seconds
 driver.implicitly_wait(10)
 
-driver.get("https://www.tokopedia.com/p/kecantikan/perawatan-wajah")
-# print(driver.title)
+# Open Tokopedia website
+driver.get("https://www.tokopedia.com/")
 
-# driver.find_element(By.XPATH, '//*[@id="header-main-wrapper"]/div[2]/div[2]/div/div/div/div/input').send_keys(
-#     "skincare\n")
+# Search for the product
+search_box = driver.find_element(By.XPATH, '//*[@id="header-main-wrapper"]/div[2]/div[2]/div/div/div/div/input')
+search_box.send_keys("face wash acne\n")
 
-
-# print(driver.current_url)
+# Wait for the page to load
+time.sleep(5)
 
 def createDataset():
-    dataset = {
-        "img_url": [],
-        "url": [],
-        "title": [],
-        "price": [],
-        "slash_price": [],
-        "disc": [],
-        "pelayan": [],
-        "rating": [],
-        "terjual": []
+    return {
+        "url": "",
+        "title": "",
+        "price": 0,
+        "rating": 0.0,
+        "terjual": 0
     }
-    return dataset
 
+def clean_price(price_str):
+    return int(price_str.replace('Rp', '').replace('.', ''))
 
-# 9 pg pertama
-for i in range(9):
-    dataset = createDataset()
-    # baris 1 (3 kolom)
-    baris1 = driver.find_element(By.CLASS_NAME, "css-1nod7cp")
-    kolom_b1 = baris1.find_elements(By.CLASS_NAME, "css-kkkpmy")
-    print(len(kolom_b1))
-    for kolom in kolom_b1:
-        img_url = kolom.find_element(By.TAG_NAME, "img").get_attribute("src")
+def clean_rating(rating_str):
+    return float(rating_str)
 
-        data_baris1 = kolom.find_element(By.CLASS_NAME, "css-1asz3by")
-        object_url = data_baris1.find_element(By.TAG_NAME, 'a').get_attribute("href")
+def clean_terjual(terjual_str):
+    terjual_str = terjual_str.replace('terjual', '').replace('+', '').strip()
+    if 'rb' in terjual_str:
+        return int(float(terjual_str.replace('rb', '')) * 1000)
+    return int(terjual_str)
 
-        object_datas = data_baris1.find_element(By.TAG_NAME, 'a').text
-        datas = object_datas.split("\n")
+arr_of_dict = []
 
-        print(datas)
+os.system('cls')
 
+for i in range(20):
+    jumlah_produk_per_halaman = 0
+    time.sleep(5)
+
+    total_height = driver.execute_script("return document.body.scrollHeight") * 10
+    for position in range(0, total_height, 100):
+        driver.execute_script(f"window.scrollTo(0, {position});")
+
+    # Locate product list container
+    all_selected = driver.find_element(By.XPATH, '//*[@id="zeus-root"]/div/div[2]/div/div[2]/div[4]')
+    lines = all_selected.find_elements(By.CLASS_NAME, "css-jza1fo")
+
+    for product in lines:
         try:
-            dataset["url"].append(object_url)
-            dataset["img_url"].append(img_url)
-            dataset["title"].append(datas[0])
-            dataset["price"].append(datas[1])
-            dataset["slash_price"].append(datas[2])
-            dataset["disc"].append(datas[3])
-            dataset["pelayan"].append(datas[4])
-            dataset["rating"].append(datas[5])
-            dataset["terjual"].append(datas[6])
+            objs = product.find_elements(By.CLASS_NAME, "css-llwpbs")
+        except:
+            continue
 
-            # dataset = pd.DataFrame(dataset)
-            print("------------------------------")
-            print(dataset)
-            dataset = createDataset()
-            print()
-        except IndexError:
-            # print("*********************")
-            pass
+        for obj in objs:
+            try:
+                product_link = obj.find_element(By.CLASS_NAME, "css-1asz3by").find_element(By.TAG_NAME, 'a')
+                url = product_link.get_attribute('href')
+                title = product_link.find_element(By.CLASS_NAME, "css-3um8ox").text
+                price_str = product_link.find_element(By.CLASS_NAME, "css-h66vau").text
+                rating_str = product_link.find_element(By.CLASS_NAME, "css-t70v7i").text
+                terjual_str = product_link.find_element(By.CLASS_NAME, "css-1sgek4h").text
 
-    # baris 2, 3
-    dataset = createDataset()
+                price = clean_price(price_str)
+                rating = clean_rating(rating_str)
+                terjual = clean_terjual(terjual_str)
 
-    baris2 = driver.find_element(By.CLASS_NAME, "css-jza1fo")
-    baris2 = baris2.find_elements(By.CLASS_NAME, "css-llwpbs")
-    for kolom in baris2:
-        img_url = kolom.find_element(By.TAG_NAME, 'a').get_attribute("href")
-        url = kolom.find_element(By.TAG_NAME, 'img').get_attribute("src")
-        datas = kolom.find_element(By.CLASS_NAME, "css-1asz3by")
-        datas = datas.find_element(By.TAG_NAME, 'a').text
-        datas = datas.split("\n")
+                print(jumlah_produk_per_halaman + 1)
+                print(url)
+                print(title)
+                print(price)
+                print(rating)
+                print(terjual)
+                print()
 
-        print(datas)
-        print()
+                dataset = createDataset()
+                dataset['url'] = url
+                dataset['title'] = title
+                dataset['price'] = price
+                dataset['rating'] = rating
+                dataset['terjual'] = terjual
+                arr_of_dict.append(dataset)
 
-        if len(datas) >= 7:
-            dataset["img_url"].append(img_url)
-            dataset["url"].append(url)
-            dataset["title"].append(datas[1])
-            dataset["price"].append(datas[2])
-            dataset["slash_price"].append(datas[3])  # salah krn tiap produk len(datas) nya beda
-            dataset["disc"].append(datas[4])
-            # dataset["pelayan"].append()
-            dataset["rating"].append(datas[-2])
-            dataset["terjual"].append(datas[-1])
+                jumlah_produk_per_halaman += 1
+            except Exception as e:
+                print(f"Error: {e}")
+                pass
 
-        dataset = createDataset()
+    total_height = driver.execute_script("return document.body.scrollHeight")
+    for position in range(total_height, 0, -100):
+        driver.execute_script(f"window.scrollTo(0, {position});")
 
-    driver.get(
-        f"https://www.tokopedia.com/search?navsource=home&page={2 + i}&q=skincare&source=universe&srp_component_id=01.02.01.01&st=product")
+    print(f"jumlah produk halaman {i + 1}: {jumlah_produk_per_halaman}")
+    driver.get(f"https://www.tokopedia.com/search?navsource=home&page={2 + i}&q=face+wash+acne&source=universe&srp_component_id=01.02.01.01&st=product")
     driver.implicitly_wait(10)
     print()
-    print(2 + i)
     print()
 
-while True:
-    pass
+# Save data to JSON file
+with open('tokopedia_data.json', 'w', encoding='utf-8') as f:
+    json.dump(arr_of_dict, f, ensure_ascii=False, indent=4)
+
+driver.quit()
+
+# while True:
+#     pass
